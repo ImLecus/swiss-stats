57b5df58ce3b7cce87ead516a3c9ef8afb0fd0c2
Marcos González <imlecus@proton.me>
2025-07-20 18:05:27 +0200

    Added MapControls

---
+++ frontend/src/components/MapControls.tsx
@@ -0,0 +1,12 @@
+import "../styles/map-controls.css"
+
+function MapControls() {
+    return (
+        <div id="map-controls">
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
+}
+
+export default MapControls;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import Sidebar from "./components/Sidebar.tsx";
 import './index.css'
+import MapControls from "./components/MapControls.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Sidebar/>
+    <MapControls />
   </StrictMode>,
 )
